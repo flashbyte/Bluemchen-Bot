500ebfa5d5d3da656691e00842770217a6f73ee4
Nils Mull <mail@flash-byte.de>
2013-02-09 19:57:02 +0100

    Debug

--- tempSensor.py
+++ tempSensor.py
@@ -6,9 +6,9 @@ import sys
 #2013/02/09 19:49:21 Temperature 76.78F 24.88C
 def getTemp(text):
 	result = re.search('\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}.*?\d{2}.\d{2}F (\d{2}.\d{2})C',text)
-	if result!='':
-		tempString = result.groups(0)
-		return double(tempString)
+	if result!=None:
+		tempString = result.groups()[0]
+		return float(tempString)
 	else:
 		return None
 
