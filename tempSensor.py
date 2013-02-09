9310b436ebd13478c76170b07d85378c69b08471
shog2 <shog2>
2013-02-09 19:52:34 +0100

    done write to db

--- tempSensor.py
+++ tempSensor.py
@@ -1,8 +1,27 @@
 #! /usr/bin/env python
-import MySQLdb
-
-db = MySQLdb.connect(host='poseidon',user='wg_bot',passwd='umpalumpa',db='wg_database')
+import pymysql
 
 
+def postTempToDB(temperatur):
 
-db.close()
+    db = pymysql.connect(host='poseidon', user='wg_bot', passwd='umpalumpa', db='wg_database')
+    cursor = db.cursor()
+
+    if temperatur:
+        sql = """INSERT INTO temperatur_sensor(temperatur)
+                 VALUES ('%f')""" % temperatur
+    else:
+        sql = """INSERT INTO temperatur_sensor(temperatur)
+                 VALUES (NULL)"""
+    try:
+        # Execute the SQL command
+        cursor.execute(sql)
+        # Commit your changes in the database
+        db.commit()
+    except:
+        # Rollback in case there is any error
+        db.rollback()
+
+    db.close()
+
+postTempToDB(None)
