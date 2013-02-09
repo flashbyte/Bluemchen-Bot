242e3333cd736d315eac3cd494bc70d86905adc5
Nils Mull <mail@flash-byte.de>
2013-02-09 19:24:53 +0100

    Added Fun to pull temperature and return it

--- tempSensor.py
+++ tempSensor.py
@@ -1,8 +1,19 @@
 #! /usr/bin/env python
 import MySQLdb
+import re
+import subprocess
 
-db = MySQLdb.connect(host='poseidon',user='wg_bot',passwd='umpalumpa',db='wg_database')
 
+def getTemp():
+	try:
+		res = subprocess.check_output(["/usr/bin/pcsensor"])
+		result = re.search('\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}.*?\d{2}.\d{2}F (\d{2}.\d{2})C',line)
+		if result != None:
+			tempString = result.group(0)
+			return tempString
+		else:
+			return ''
+	except:
+		return ''
 
-
-db.close()
+print getTemp()
