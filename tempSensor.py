cb2af1612dedef26de871a2a81f232ab2dcd4196
Nils Mull <mail@flash-byte.de>
2013-02-09 19:39:51 +0100

    Debug

--- tempSensor.py
+++ tempSensor.py
@@ -7,7 +7,7 @@ import subprocess
 def getTemp():
 	try:
 		res = subprocess.check_output(["/usr/bin/pcsensor"])
-		result = re.search('\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}.*?\d{2}.\d{2}F (\d{2}.\d{2})C',line)
+		result = re.search('\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}.*?\d{2}.\d{2}F (\d{2}.\d{2})C',res)
 		print result
 		if result != None:
 			tempString = result.group(0)
