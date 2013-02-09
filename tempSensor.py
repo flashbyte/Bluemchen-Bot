296b963dc739fdd75542f288092f726437f97769
Nils Mull <mail@flash-byte.de>
2013-02-09 19:51:11 +0100

    Debug

--- tempSensor.py
+++ tempSensor.py
@@ -3,10 +3,10 @@ import MySQLdb
 import re
 import sys
 
-
+#2013/02/09 19:49:21 Temperature 76.78F 24.88C
 def getTemp(text):
 	result = re.search('\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}.*?\d{2}.\d{2}F (\d{2}.\d{2})C',text)
-	if result:
+	if result!='':
 		tempString = result.group(0)
 		return double(tempString)
 	else:
