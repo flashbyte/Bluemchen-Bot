f3a6077da27dc069792f3670cee41c6ef8a4b2ea
Nils Mull <mail@flash-byte.de>
2013-02-23 21:26:30 +0100

    PET8 conform

--- webhandler.py
+++ webhandler.py
@@ -1,10 +1,11 @@
-
+#from mod_python import apache
 import tempSens
 
-def getGraph(req,houres=24,title='Temperatur',xSize=800,ySize=600):
-    text = "You will get an Graph with size %sx%s and the Title:%s which will show the last %s houres" %(xSize,ySize,title,houres)
+
+def getGraph(req, houres=24, title='Temperatur', xSize=800, ySize=600):
+    #text = "You will get an Graph with size %sx%s and the Title:%s which will show the last %s houres" % (xSize, ySize, title, houres)
     g = tempSens.tempSensor()
-    g.plotToFile(houres,'/tmp/test.png','WebTest')
+    g.plotToFile(houres, '/tmp/test.png', 'WebTest')
     req.sendfile('/tmp/test.png')
 
 
