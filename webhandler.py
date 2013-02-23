29680e09871abab00533fe20fe4010f2e79a13dc
Nils Mull <mail@flash-byte.de>
2013-02-23 22:54:29 +0100

    Request sends png now

--- webhandler.py
+++ webhandler.py
@@ -6,6 +6,5 @@ def getGraph(req, houres=24, title='Temperatur', xSize=800, ySize=600):
     #text = "You will get an Graph with size %sx%s and the Title:%s which will show the last %s houres" % (xSize, ySize, title, houres)
     g = tempSens.tempSensor()
     g.plotToFile(houres, '/tmp/test.png', 'WebTest')
+    req.content_type = 'image/png'
     req.sendfile('/tmp/test.png')
-
-
