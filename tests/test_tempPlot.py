aa149777741a46866932a13ce9c7d7d21d424f25
Nils Mull <mail@flash-byte.de>
2013-02-27 14:33:00 +0100

    Added some Tests

--- tests/test_tempPlot.py
+++ tests/test_tempPlot.py
@@ -4,14 +4,24 @@ import unittest
 from tempPlot import tempPlot
 
 
+#Todo: Create Test DB
 class test_tempPlot(unittest.TestCase):
     def setUp(self):
+        self.plot = tempPlot()
         pass
 
     def tearDown(self):
         pass
 
     def test_getData(self):
-        plot = tempPlot()
-        data = plot.__getData__(10)
+        data = self.plot.__getData__(10)
         self.assertGreater(len(data), 0)
+
+    def test_getData_noData(self):
+        data = self.plot.__getData__(0)
+        self.assertEqual(data, None)
+
+    def test_getData_minus(self):
+        data = self.plot.__getData__(-10)
+        self.assertEqual(data, None)
+
